d62f00589796958a1e100a61e6bdcc049a2c4546
myucoco <93854701+myucoco@users.noreply.github.com>
2021-11-12 22:40:53 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,70 @@
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+const CACHE_NAME = `v1`;
+
+// キャッシュするファイルをセット
+const urlsToCache = [
+  './',
+  './style.css',
+  './cocoicon192.png',
+  './cocoicon256.png',
+  './index.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
- 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!CACHE_NAME.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log(CACHE_NAME + "activated");
+    })
+  );
 });
- 
-// サービスワーカー有効化に必須
-self.addEventListener('fetch', function(event) {});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+    .then((response) => {
+      // キャッシュ内に該当レスポンスがあれば、それを返す
+      if (response) {
+        return response;
+      }
+
+      // 重要：リクエストを clone する。リクエストは Stream なので
+      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+      // 必要なので、リクエストは clone しないといけない
+      let fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest)
+        .then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            // キャッシュする必要のないタイプのレスポンスならそのまま返す
+            return response;
+          }
+
+          // 重要：レスポンスを clone する。レスポンスは Stream で
+          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+          // 2つの Stream があるようにする
+          let responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+    })
+  );
+});
